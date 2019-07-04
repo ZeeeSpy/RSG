aba3947274a9f2a154d23d1413939eac2245a218
IliaBahmutov <52408039+IliaBahmutov@users.noreply.github.com>
2019-07-04 22:34:59 +0100

    0.0.5
    South room created with camera room switching script

---
+++ RSG/Assets/Scripts/RoomCameraToggle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomCameraToggle : MonoBehaviour
+{
+
+    public Camera previouscam; //camera from previous room
+    public Camera targetcam; //camera in target room
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) //if collision happens with the player switch cameras, two colliders needed one for each way
+        {
+            previouscam.enabled = false;
+            targetcam.enabled = true;
+        } 
+    }
+}
