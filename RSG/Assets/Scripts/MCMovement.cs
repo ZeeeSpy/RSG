0b04464db22eb09ccdca8cdadbd88a2696ac93fd
IliaBahmutov <52408039+IliaBahmutov@users.noreply.github.com>
2019-07-04 16:04:10 +0100

    0.0.3
    Switched to physics based movement. Added world titlemap and collision.

--- RSG/Assets/Scripts/MCMovement.cs
+++ RSG/Assets/Scripts/MCMovement.cs
@@ -7,6 +7,8 @@ public class MCMovement : MonoBehaviour
     private float charspeed = 0.8f;
     public Animator animator;
 
+    public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +18,12 @@ public class MCMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3((Input.GetAxis("Horizontal") * (charspeed)), Input.GetAxis("Vertical") *(charspeed), 0.0f);
+        Vector3 movement = new Vector3((Input.GetAxis("Horizontal") * (charspeed)), Input.GetAxis("Vertical") * (charspeed), 0.0f);
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Magnitude", movement.magnitude);
 
-        transform.position = transform.position + movement * Time.deltaTime;
+        rb.velocity = new Vector2(movement.x, movement.y);
     }
 }
