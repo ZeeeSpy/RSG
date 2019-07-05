1730d3b8a7a034adb194e932687ef46e7761da47
IliaBahmutov <52408039+IliaBahmutov@users.noreply.github.com>
2019-07-05 21:08:50 +0100

    0.0.7
    Pathfining System Added

---
+++ RSG/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyScript : MonoBehaviour
+{
+    //Pathfinding
+    public Transform target;
+    public float speed = 200f;
+    public float nextWaypoitnDistance = 3f;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndofPath = false;
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        //pathfinding setup
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       if (path == null)
+        {
+            return;
+        }
+
+       if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndofPath = true;
+            return;
+        } else
+        {
+            reachedEndofPath = false;
+        }
+
+       Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+       Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypoitnDistance)
+        {
+            currentWaypoint++;
+        }
+    }
+
+}
