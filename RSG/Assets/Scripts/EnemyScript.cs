37fa3c18c1c3a7c077d816dbd6facfe0ab6f06b8
IliaBahmutov <52408039+IliaBahmutov@users.noreply.github.com>
2019-07-06 13:04:27 +0100

    0.0.8b
    3 pont patrol ability added to enemy AI

--- RSG/Assets/Scripts/EnemyScript.cs
+++ RSG/Assets/Scripts/EnemyScript.cs
@@ -6,6 +6,7 @@ using Pathfinding;
 public class EnemyScript : MonoBehaviour
 {
     //Pathfinding
+    public Transform player;
     public Transform target;
     public float speed = 75f;
     public float nextWaypoitnDistance = 1f;
@@ -17,19 +18,36 @@ public class EnemyScript : MonoBehaviour
     Rigidbody2D rb;
     public Transform viewcone;
 
+
+    //Patrol points
+    private int patrolcount = 0;
+    public Transform patrol1;
+    public Transform patrol2;
+    public Transform patrol3;
+    private Transform[] patrolpoints = new Transform[3];
+
     void Start()
     {
+        //patrol setup
+        patrolpoints[0] = patrol1;
+        patrolpoints[1] = patrol2;
+        patrolpoints[2] = patrol3;
+        target = patrolpoints[patrolcount];
         //pathfinding setup
         seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
-        InvokeRepeating("UpdatePath", 0f, .1f);
         lookahead = Vector2.zero;
+        InvokeRepeating("UpdatePath", 0f, 0.1f);
     }
 
     void UpdatePath()
     {
         if (seeker.IsDone())
         {
+            if (reachedEndofPath)
+            {
+                cyclepatrol();
+            }
             seeker.StartPath(rb.position, target.position, OnPathComplete);
         }
     }
@@ -46,6 +64,7 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+
        if (path == null)
         {
             return;
@@ -69,7 +88,7 @@ public class EnemyScript : MonoBehaviour
             //look ahead 4 waypoints, calculate angle of target relative to enemy, lerp between current cone position and look ahead position
             lookahead = ((Vector2)path.vectorPath[currentWaypoint + 5] - rb.position).normalized;
             float rotationZ = Mathf.Atan2(lookahead.y * 10, lookahead.x * 10) * Mathf.Rad2Deg;
-            viewcone.rotation = Quaternion.Lerp(viewcone.rotation, Quaternion.Euler(0.0f, 0.0f, rotationZ + 90), 5f*Time.deltaTime);
+            viewcone.rotation = Quaternion.Slerp(viewcone.rotation, Quaternion.Euler(0.0f, 0.0f, rotationZ + 90), 5f*Time.deltaTime);
         } catch
         {
             //exepcted behvaiour do nothing.when within 4 waypoints (0.24 in game units, the enemy will not look elsewhere)
@@ -83,4 +102,14 @@ public class EnemyScript : MonoBehaviour
         }
     }
 
+
+    private void cyclepatrol()
+    {
+        patrolcount++;
+        if (patrolcount == 3)
+        {
+            patrolcount = 0;
+        }
+        target = patrolpoints[patrolcount];
+    }
 }
