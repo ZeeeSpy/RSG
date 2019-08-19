f9174077dbc618b5b484c0a6b1d2cf1f1c21468d
IliaBahmutov <52408039+IliaBahmutov@users.noreply.github.com>
2019-08-19 17:38:55 +0100

    0.0.42
    Fixed camera aliasing issue, started work on L1-1.

--- RSG/Assets/Scripts/Camera Scripts/CameraFollow.cs
+++ RSG/Assets/Scripts/Camera Scripts/CameraFollow.cs
@@ -11,13 +11,14 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform player;
     Vector3 velocity = Vector3.zero;
+
     private float smoothTime = 0.2f;
     public float yMinValue;
     public float yMaxValue;
     public float xMinValue;
     public float xMaxValue;
-    private float pixelsize = 100;
-    public float lookaheaddist = 1;
+    public float pixelsize = 50;
+    public float lookaheaddist = 1.1f;
 
 
     // Update is called once per frame
@@ -30,9 +31,16 @@ public class CameraFollow : MonoBehaviour
 
 
         transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
-
-        transform.position = new Vector3((Mathf.RoundToInt(gameObject.transform.position.x * pixelsize) / pixelsize),
-                                        (Mathf.RoundToInt(gameObject.transform.position.y * pixelsize) / pixelsize),
+        /*
+         * Art assets are 100pixels per unit. 
+         * 
+         * Trying to make the camera pixel perfect (Correcting to 0.00) causes aliasing issues.
+         * Solution:
+         * This makes the camera snap to a precision of 0.02 and then add 0.01 to make the number odd.
+         * since for some reason unknown to god himself unity doesn't like even numbers
+         */
+        transform.position = new Vector3((Mathf.RoundToInt(gameObject.transform.position.x * pixelsize) / pixelsize)+0.01f, 
+                                        (Mathf.RoundToInt(gameObject.transform.position.y * pixelsize) / pixelsize)+0.01f,
                                         (Mathf.RoundToInt(gameObject.transform.position.z * pixelsize) / pixelsize));
     }
 }
