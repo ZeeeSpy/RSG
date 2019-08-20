1ee6c9a5702da5f923ed307213aafc671537ef4b
IliaBahmutov <52408039+IliaBahmutov@users.noreply.github.com>
2019-08-20 14:18:48 +0100

    0.0.43
    Fixed camera stutter/pixel aliasing again. Started work on main objectives of L1-1 and L1-1F2

--- RSG/Assets/Scripts/Camera Scripts/CameraFollow.cs
+++ RSG/Assets/Scripts/Camera Scripts/CameraFollow.cs
@@ -17,12 +17,13 @@ public class CameraFollow : MonoBehaviour
     public float yMaxValue;
     public float xMinValue;
     public float xMaxValue;
-    public float pixelsize = 50;
-    public float lookaheaddist = 1.1f;
+    private float pixelsize = 100;
+    private float ypixelsize = 75;
+    private float lookaheaddist = 1.1f;
 
 
     // Update is called once per frame
-    void FixedUpdate()
+    void LateUpdate()
     {
         Vector3 targetPos = player.position + new Vector3(Input.GetAxis("CamHor") * (lookaheaddist), Input.GetAxis("CamVer") * (lookaheaddist), -10f); ;
 
@@ -39,8 +40,8 @@ public class CameraFollow : MonoBehaviour
          * This makes the camera snap to a precision of 0.02 and then add 0.01 to make the number odd.
          * since for some reason unknown to god himself unity doesn't like even numbers
          */
-        transform.position = new Vector3((Mathf.RoundToInt(gameObject.transform.position.x * pixelsize) / pixelsize)+0.01f, 
-                                        (Mathf.RoundToInt(gameObject.transform.position.y * pixelsize) / pixelsize)+0.01f,
+        transform.position = new Vector3((Mathf.RoundToInt(gameObject.transform.position.x * pixelsize) / pixelsize), 
+                                        (Mathf.RoundToInt(gameObject.transform.position.y * ypixelsize) / ypixelsize),
                                         (Mathf.RoundToInt(gameObject.transform.position.z * pixelsize) / pixelsize));
     }
 }
